a4ea7ca215168387d77c64ded3a950bdc68ae7df
Joey <joeybb@MacBook-Pro.local>
2017-04-20 15:42:47 -0500

    css and js edits

--- cards.js
+++ cards.js
@@ -2,10 +2,15 @@ console.log("cards.js");
 
 var textArea = document.getElementById("text_area");
 var card = document.getElementById("card");
+// var node = document.createTextNode("div");
+// node.appendChild("newCard");
+// document.getElementById("card").appendChild(node)
+
 var button = document.getElementById("submit").addEventListener("click", function(submit){
 	console.log("userText", textArea.value);
 
 })
+
 var deleteCard = document.getElementById("delete").addEventListener("click", function(removeItem){
 	console.log("delete button works now.");
-})
+});
